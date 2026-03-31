add722ee1a02aca99b335c9e3dc2f201be54627c
coolzyd9107 <35447134+coolzyd9107@users.noreply.github.com>
2026-03-31 20:37:34 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Wild Kernels by TheWildJames aka Morgan Weedman
+kernel.string=ReSukiSU community kernel builds by coolzyd9107 aka zhuzhuzihan aka huangdihd
 do.devicecheck=0
 do.modules=0
 do.systemless=0
@@ -40,7 +40,7 @@ case $kernel_version in
     *) ksu_supported=false ;;
 esac
 
-ui_print " " "  -> Wild Kernels Supported: $ksu_supported"
+ui_print " " "  -> GKI Kernel Supported: $ksu_supported"
 $ksu_supported || abort "  -> Non-GKI device, abort."
 
 # boot install
@@ -53,25 +53,14 @@ else
 fi
 
 ui_print " "
-ui_print "WildKernels Telegram Channel:"
-ui_print "https://t.me/WildKernels"
-ui_print " "
-ui_print "WildKernels Website:"
-ui_print "https://wildkernels.dev"
-ui_print " "
-ui_print "Wild_KSU GitHub Repository:"
-ui_print "https://github.com/WildKernels/Wild_KSU"
-ui_print "KernelSU-Next fork focused on customization and root-hiding features!"
+ui_print "Our Telegram Channel:"
+ui_print "https://t.me/ReSukiSUKernelBuilds"
 ui_print " "
 ui_print "GKI_KernelSU_SUSFS GitHub Repository:"
-ui_print "https://github.com/WildKernels/GKI_KernelSU_SUSFS"
+ui_print "https://github.com/coolzyd9107/GKI_KernelSU_SUSFS"
 ui_print "GKI kernels with KernelSU and SUSFS."
 ui_print " "
 ui_print "OnePlus_KernelSU_SUSFS GitHub Repository:"
-ui_print "https://github.com/WildKernels/OnePlus_KernelSU_SUSFS"
+ui_print "https://github.com/huangdihd/OnePlus_ReSukiSU_SUSFS"
 ui_print "OnePlus kernels with KernelSU and SUSFS."
-ui_print " "
-ui_print "Samsung_KernelSU_SUSFS GitHub Repository:"
-ui_print "https://github.com/WildKernels/Samsung_KernelSU_SUSFS"
-ui_print "Samsung kernels with KernelSU and SUSFS."
 ui_print " "
